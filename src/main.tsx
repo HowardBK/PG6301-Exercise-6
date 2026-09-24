a5541870dc5b4b009d68e95fb01c2d24d6ae2421
HowardBK <dazmhbk@gmail.com>
2026-09-24 14:52:42 +0200

    Create initial task application front page

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
+function Application() {
+  const [tasks, setTasks] = useState([
+    { description: "Create project", completed: true },
+    { description: "Create React webapp", completed: false },
+    { description: "Create Hono backend", completed: false },
+  ]);
+  return (
+    <>
+      <h1>My Task Manager</h1>
+      <ul>
+        {tasks.map((t) => (
+          <li>{t.description}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
 createRoot(document.getElementById("root")!).render(
-  <h1>Hello There Slowcoders</h1>,
+  <h1>
+    <Application />
+  </h1>,
 );
